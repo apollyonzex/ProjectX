a28558053e57b67620441918318e67545e02cfef
zhangjunchi <zhangjunchi@conchship.net>
2023-10-24 18:29:24 +0800

    --Add 1. trigger模板。 2. 手牌文件夹。

---
+++ Assets/Editor/AutoCreators/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+
+namespace Editor.AutoCreators
+{
+    public class Trigger
+    {
+        static string path = "Assets/Editor/Templates/Trigger_Template.cs.txt";
+
+        //==================================================================================================
+
+        [MenuItem("Assets/AutoScript/Trigger", false, -1)]
+        public static void EXE()
+        {
+            CreateScriptByTemplate.EXE(path, create_file_name);
+        }
+
+
+        static string create_file_name(string folder_name)
+        {
+            return $"{folder_name.TrimEnd('s')}Trigger";
+        }
+    }
+}
+
